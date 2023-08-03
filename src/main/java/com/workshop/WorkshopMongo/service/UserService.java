f5dbce69eb7b33f22b735e6813c464640210d47a
Luisa Araujo <luisaaraujo218@gmail.com>
2023-08-03 11:21:20 -0300

    método findById

--- src/main/java/com/workshop/WorkshopMongo/service/UserService.java
+++ src/main/java/com/workshop/WorkshopMongo/service/UserService.java
@@ -2,10 +2,12 @@ package com.workshop.WorkshopMongo.service;
 
 import com.workshop.WorkshopMongo.domain.User;
 import com.workshop.WorkshopMongo.repository.UserRepository;
+import com.workshop.WorkshopMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,8 +15,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
+
