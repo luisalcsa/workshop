a9ba1741bb86256718b6d00c1539cfdf956bcc14
Luisa Araujo <luisaaraujo218@gmail.com>
2023-08-09 12:25:01 -0300

    consulta com query methods

--- src/main/java/com/workshop/WorkshopMongo/resources/PostsResources.java
+++ src/main/java/com/workshop/WorkshopMongo/resources/PostsResources.java
@@ -3,6 +3,7 @@ package com.workshop.WorkshopMongo.resources;
 import com.workshop.WorkshopMongo.domain.Post;
 import com.workshop.WorkshopMongo.domain.User;
 import com.workshop.WorkshopMongo.dto.UserDto;
+import com.workshop.WorkshopMongo.resources.util.URL;
 import com.workshop.WorkshopMongo.service.PostService;
 import com.workshop.WorkshopMongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,12 +21,21 @@ import java.util.stream.Collectors;
 public class PostsResources {
 
     @Autowired
-    private PostService postServiceService;
+    private PostService postService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id){
-        Post post = postServiceService.findById(id);
+        Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
     }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = " ") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 
 }
--- src/main/java/com/workshop/WorkshopMongo/service/PostService.java
+++ src/main/java/com/workshop/WorkshopMongo/service/PostService.java
@@ -23,4 +23,8 @@ public class PostService {
         return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
     }
 
+    public List<Post> findByTitle(String text){
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
+
 }
